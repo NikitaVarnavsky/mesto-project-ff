7dbf6fa679e6ac8a77671948e5bad31be4606a89
Nikita Varnavsky <varnavskynik@yandex.ru>
2025-04-14 16:59:31 +0300

    second iteration

--- scripts/index.js
+++ scripts/index.js
@@ -5,25 +5,27 @@ const cardTemplateContent = document.querySelector('#card-template').content;
 const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-const createCard = (currentCard, cardValue) => {
-  currentCard.querySelector('.card__image').src = cardValue.link;
-  currentCard.querySelector('.card__title').textContent = cardValue.name;
-  cardsList.append(currentCard);
-};
-
-// @todo: Функция удаления карточки
-const deleteCard = (item) => {
-  const currentCard = item.parentElement;
-  currentCard.remove();
-};
-
-// @todo: Вывести карточки на страницу
-
-initialCards.map((card) => {
+const createCard = (cardValue, deleteCard) => {
   const cloneCard = cardTemplateContent.querySelector('.card').cloneNode(true);
   const deleteCardButton = cloneCard.querySelector('.card__delete-button');
   deleteCardButton.addEventListener('click', (event) => {
     deleteCard(event.target);
   });
-  createCard(cloneCard, card);
+  cloneCard.querySelector('.card__image').src = cardValue.link;
+  cloneCard.querySelector('.card__image').alt = `Фотография места: ${cardValue.name}`;
+  cloneCard.querySelector('.card__title').textContent = cardValue.name;
+  return cloneCard;
+};
+
+// @todo: Функция удаления карточки
+const deleteCard = (item) => {
+  const currentCard = item.closest('.places__item');
+  currentCard.remove();
+};
+
+// @todo: Вывести карточки на страницу
+
+initialCards.forEach((item) => {
+  const card = createCard(item, deleteCard);
+  cardsList.append(card);
 });
